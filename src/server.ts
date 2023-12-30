a0095ab6db646c4a49ca79aaf2ff6bbe2db1b5f0
Patrick M <patrick@paiger.co>
2023-12-30 20:40:14 +0000

    Remove boobs, print logo on server start

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import fs from "node:fs";
 import { appsManger } from "./core";
 import EventEmitter from "node:events";
 import xpipe from "xpipe";
+import { catImage } from "./utils/cat";
+import chalk from "chalk";
 
 // apps should be given an even emitter
 
@@ -13,49 +15,60 @@ const parseData = (data: string) => {
 };
 
 const sock = xpipe.eq("/tmp/wabsnet");
-const server = net.createServer({ keepAlive: true }, (c) => {
-    const emitter = new EventEmitter();
-    console.log("client connected");
-    c.on("end", () => {
-        console.log("client disconnected");
-        emitter.emit("close");
-        c.end();
-    });
-    c.on("data", async (d) => {
-        const data = parseData(d.toString().trim());
-        emitter.on("msg", (text) => c.write(JSON.stringify([null, Buffer.from(text).toString("base64")])));
-        emitter.on("end", () => c.end());
-        emitter.on("error", (err) => c.write(JSON.stringify([err, null])));
-        const app = data.commands.shift();
-        if (!app) {
-            emitter.emit("error", "Command not recognised");
-            emitter.emit("end");
-            return;
-        }
-        appsManger.execApp(app, data, emitter);
-    });
-});
 
-server.on('error', function (e: NodeJS.ErrnoException) {
-    if (e.code == 'EADDRINUSE') {
-        var clientSocket = new net.Socket();
-        clientSocket.on('error', function(e: NodeJS.ErrnoException) { // handle error trying to talk to server
-            if (e.code == 'ECONNREFUSED') {  // No other server listening
-                fs.unlinkSync(sock);
-                server.listen(sock, function() { //'listening' listener
-                    console.log('server recovered');
-                });
+async function run() {
+    const logo = await catImage("logo.png", 60);
+    console.log("\n\n");
+    console.log(logo);
+    console.log(chalk.green.bold("\n\n                    WABSNET v.1.933"));
+    console.log("\n\n");
+
+    const server = net.createServer({ keepAlive: true }, (c) => {
+        const emitter = new EventEmitter();
+        console.log("client connected");
+        c.on("end", () => {
+            console.log("client disconnected");
+            emitter.emit("close");
+            c.end();
+        });
+        c.on("data", async (d) => {
+            const data = parseData(d.toString().trim());
+            emitter.on("msg", (text) => c.write(JSON.stringify([null, Buffer.from(text).toString("base64")])));
+            emitter.on("end", () => c.end());
+            emitter.on("error", (err) => c.write(JSON.stringify([err, null])));
+            const app = data.commands.shift();
+            if (!app) {
+                emitter.emit("error", "Command not recognised");
+                emitter.emit("end");
+                return;
             }
+            appsManger.execApp(app, data, emitter);
         });
-        clientSocket.connect({path: sock}, function() {
-            console.log('Server running, giving up...');
-            process.exit();
-        });
-    }
-});
+    });
 
-server.listen(sock, () => {
-    console.log(`listening to ${sock}`);
-});
+    server.on('error', function (e: NodeJS.ErrnoException) {
+        if (e.code == 'EADDRINUSE') {
+            var clientSocket = new net.Socket();
+            clientSocket.on('error', function(e: NodeJS.ErrnoException) { // handle error trying to talk to server
+                if (e.code == 'ECONNREFUSED') {  // No other server listening
+                    fs.unlinkSync(sock);
+                    server.listen(sock, function() { //'listening' listener
+                        console.log('server recovered');
+                    });
+                }
+            });
+            clientSocket.connect({path: sock}, function() {
+                console.log('Server running, giving up...');
+                process.exit();
+            });
+        }
+    });
 
-process.on("beforeExit", () => server.close());
+    server.listen(sock, () => {
+        console.log(`listening to ${sock}`);
+    });
+    process.on("beforeExit", () => server.close());
+}
+
+run();
+
